c7f8e32e75a22ea775bacf75e92aa279a5e253aa
exp <exp.table@protonmail.com>
2021-09-24 12:47:57 +0200

    Discrete dutch auction

---
+++ contracts/DiscreteDutchAuction.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.0;
+
+contract DiscreteDutchAuction {
+
+    struct Auction {
+        uint256 startingPrice;
+        uint64 startingBlock;
+        uint64 decreasingConstant;
+        uint64 steps;
+        uint64 interval;
+    }
+
+    mapping (uint => Auction) public auctions;
+
+    uint256 private nextAuctionId;
+
+    function createAuction(
+        uint256 startingPrice_,
+        uint64 startingBlock_,
+        uint64 decreasingConstant_,
+        uint64 steps_, // # of total decrements
+        uint64 interval_ //blocks between each decrement
+    ) public {
+        auctions[nextAuctionId++] = Auction(startingPrice_, startingBlock_, decreasingConstant_, steps_, interval_);
+    }
+
+    function getPrice(uint256 auctionId) public view returns (uint256) {
+        Auction memory auction = auctions[auctionId];
+        uint256 price = auction.startingPrice;
+        uint64 step = auction.startingBlock; //dictates price
+        for(uint256 i = 0; i < auction.steps; i++) {
+            if (block.number > step + auction.interval) {
+                step += auction.interval;
+                price -= auction.decreasingConstant;
+            }
+        }
+        return price;
+    }
+
+    function purchase(uint256 auctionId) public payable {
+        Auction memory auction = auctions[auctionId];
+        require(auction.startingBlock >= block.number, "PURCHASE:AUCTION NOT STARTED");
+        uint256 price = getPrice(auctionId);
+        require(msg.value == price, "PURCHASE:INCORRECT MSG.VALUE");
+        //do more things
+    }
+}
---
+++ test/discrete-dutch.js
@@ -0,0 +1,67 @@
+const { expect } = require("chai");
+
+function toWei(price) {
+  return (price * 10 ** 18).toString();
+}
+
+async function mineBlocks(blockNumber) {
+  while (blockNumber > 0) {
+    blockNumber--;
+    await ethers.provider.send("evm_mine");
+  }
+}
+
+function priceAfterIntervals(initialPrice, decFactor, intervals) {
+    const price = initialPrice - (intervals * decFactor);
+    return toWei(price);
+}
+
+describe("Discrete Dutch Auction", function () {
+  const initialPrice = 5;
+  const decFactor = 0.5; //decreasing factor
+  const steps = 8;
+  const interval = 50; //50 blocks interval
+  let start, dutch;
+
+
+  /*
+    i = interval
+
+    blocks: x ---> x + i ---> x + i * 1 ---> ... ---> x + i * steps ---> no change
+    price : 5      4.5        4                       1                  1
+  */
+
+  before(async function () {
+    const DDA = await ethers.getContractFactory("DiscreteDutchAuction");
+    dutch = await DDA.deploy();
+    await dutch.deployed();
+
+    start = await ethers.provider.getBlockNumber();
+
+    await dutch.createAuction(toWei(initialPrice), start, toWei(decFactor), steps, interval);
+  });
+
+  it("Initial price check", async function () {
+      const price = await dutch.getPrice(0);
+      expect(price).to.be.equal(priceAfterIntervals(initialPrice, decFactor, 0));
+  });
+
+  it("Price check on first node x+interval", async function() {
+    mineBlocks(interval);
+    const price = await dutch.getPrice(0);
+    expect(price).to.be.equal(priceAfterIntervals(initialPrice, decFactor, 1));
+  });
+
+  it("Price check on first node x+interval + middle of it", async function() {
+    mineBlocks(interval / 2);
+    const price = await dutch.getPrice(0);
+    expect(price).to.be.equal(priceAfterIntervals(initialPrice, decFactor, 1));
+  });
+
+  it("Price check after last step x + steps * interval", async function() {
+    mineBlocks(interval * steps);
+    const price = await dutch.getPrice(0);
+    expect(price).to.be.equal(priceAfterIntervals(initialPrice, decFactor, steps));
+  });
+
+});
