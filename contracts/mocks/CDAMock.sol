b9eca61878cc9669634bedd50a3195ee9cdc3427
meirbank <meir@meirbank.com>
2021-10-13 20:30:35 -0400

    Fix tests

--- contracts/mocks/CDAMock.sol
+++ contracts/mocks/CDAMock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "../auctions/ContinuousDutchAuction.sol";
 
-contract CDAMock is ContinuousDutchAuction {
+contract CDAMock is AContinuousDutchAuction {
 
     function buy(uint auctionId) public payable {
         verifyBid(auctionId);
